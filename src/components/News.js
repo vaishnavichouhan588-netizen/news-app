89c12bb682f7dee3b7726b6981a5f5fe83554be8
vaishnavichouhan588-netizen <vaishnavichouhan588@gmail.com>
2026-06-06 17:17:06 +0530

    first commit

---
+++ src/components/News.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import PropTypes, { string } from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroll-component';
+export class News extends Component{
+       
+  static defaultProps ={
+     q: 'in',
+     pageSize: 10,
+     category:'general'
+  }
+    static propTypes ={
+     q: PropTypes.string,
+     pageSize: PropTypes.number,
+     category: PropTypes.string,
+    }
+    capital = (string) =>{
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+   constructor(props){
+    super(props);
+    console.log("ello i am vaish");
+    this.state={
+    articles: [],
+    loading: true,
+    page:1,
+    totalResults:0
+     }
+     document.title=`${this.capital(this.props.category)} - NewsNova`;
+  }
+  async updateNews(){
+    this.props.setProgress(10);
+    const url =`https://newsapi.org/v2/top-headlines?q=${this.props.q}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+   this.setState({loading:true});
+    
+   let data = await fetch(url);
+     this.props.setProgress(30);
+   let parseData = await data.json()
+  this.props.setProgress(50);
+   console.log(parseData);
+   this.setState({articles: parseData.articles, totalResults: parseData.totalResults, loading:false, })
+   this.props.setProgress(100);
+  }
+ async componentDidMount(){
+  this.props.setProgress(10);
+   let url =`https://newsapi.org/v2/top-headlines?q=${this.props.q}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+   this.setState({loading:true});
+   let data = await fetch(url);
+   this.props.setProgress(60);
+   let parseData = await data.json()
+   console.log(parseData);
+   this.setState({articles: parseData.articles, totalResults: parseData.totalResults, loading:false})
+   this.props.setProgress(100);
+  }
+  handlePreviousClick= async()=>{
+  //  
+  this.setState({page: this.state.page- 1});
+  this.updateNews();
+  }
+  handleNextClick= async()=>{
+  //   if(!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
+  //  let url =`https://newsapi.org/v2/top-headlines?q=${this.props.q}&category=${this.props.category}&apiKey=178199c489784e06817c1f92703deb2f&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+  //  this.setState({loading: true});
+  //  let data = await fetch(url);
+  //  let parseData = await data.json()
+  //  this.setState({
+  //   page: this.state.page + 1,
+  //   articles: parseData.articles,
+  //   loading: false
+
+  // })
+  // }
+      this.setState({page: this.state.page + 1});
+      this.updateNews();
+}
+ 
+   fetchMoreData =async ()=>{
+    this.setState({page: this.state.page+1})
+     const url =`https://newsapi.org/v2/top-headlines?q=${this.props.q}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+   let data = await fetch(url);
+   let parseData = await data.json()
+   console.log(parseData);
+   this.setState({articles: this.state.articles.concat(parseData.articles), totalResults: parseData.totalResults })
+   };
+
+  render() {
+  
+    return (
+      <>
+        <h2 className="text-center" style={{margin:'35px 0px', marginTop:'90px'}}>NewsNova - Top {this.capital(this.props.category)} Headlines</h2>
+       { this.state.loading &&<Spinner/>} 
+        <InfiniteScroll 
+        dataLength={this.state.articles.length}
+        next={this.fetchMoreData}
+        hasMore={this.state.articles.length !== this.state.totalResults}
+        loader={<Spinner/>}
+        >
+          <div className="container">
+           
+          
+           <div className= "row">
+           { this.state.articles?.map((element)=>{
+              return    <div className="col-md-4" key={element.url}>
+                <NewsItem   title={(element.title)}  description={(element.description)}
+                 imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
+                </div>
+
+           })}
+         </div>
+        </div>
+         </InfiniteScroll>
+        {/* <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1}type="button" className="btn btn-dark" onClick={this.handlePreviousClick}> &larr; Previous</button>
+        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)}type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+        </div> */}
+      </>
+    )
+  }
+}
+export default News
---
+++ src/components/NewsItem.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+ 
+  
+  render() {
+    let{title, description,imageUrl,newsUrl, author, date, source}=this.props;
+    return (
+      <div className="my-3">
+       <div className="card" >  
+        <div style={ { display:'flex' ,
+          justifyContent: 'flex-end', 
+          position: 'absolute' ,
+          right: '0'
+
+        }} >
+          <span className= " badge rounded-pill bg-danger" >{source}</span>
+        </div>
+        <img src={!imageUrl?"https://img.etimg.com/thumb/msid-131420591,width-650,height-488,imgsize-92794,resizemode-75/sudharsan-gill-54-runs-away-from-best-ipl-season-by-batting-pair.jpg":imageUrl} className="card-img-top" alt="..."/>
+        <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+           <p className="card-text">{description}</p>
+            <p className="card-text"><small className="text-danger">By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
+            <a rel="noreferrer" href={newsUrl}target="_blank" className="btn btn-sm btn-dark">Read More</a>
+        </div>  
+    </div>
+      </div>
+    )
+  }
+}
+
+export default NewsItem
