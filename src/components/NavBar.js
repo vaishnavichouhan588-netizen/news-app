7694bcee6fccc20e3d7dc9aec3a2e7ae75158a9a
vaishnavichouhan588-netizen <vaishnavichouhan588@gmail.com>
2026-06-06 17:40:06 +0530

    fix navbar Link issue

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,7 +13,7 @@ export default class NavBar extends Component {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item"> <a className="nav-link " aria-current="page" to="/">Home</a>
+                    <li className="nav-item"> <Link className="nav-link " aria-current="page" to="/">Home</Link>
                     </li>
                   
                     <li className="nav-item"> <Link className="nav-link" to="/business">Business</Link></li>
